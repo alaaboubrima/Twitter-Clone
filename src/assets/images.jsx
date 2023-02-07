375680910ca871e82e30dc5f52968bca844fb7a1
alaaboubrima <97070504+alaaboubrima@users.noreply.github.com>
2023-02-07 21:52:18 +0100

    update

---
+++ src/assets/images.jsx
@@ -0,0 +1,38 @@
+import home from "./images/home.png"
+import home_active from "./images/home-active.png"
+import list from "./images/list.png"
+import list_active from "./images/list-active.png"
+import message from "./images/message.png"
+import message_active from "./images/message-active.png"
+import more from "./images/more.png"
+import notification from "./images/notification.png"
+import notification_active from "./images/notification-active.png"
+import profile from "./images/profile.png"
+import profile_active from "./images/profile-active.png"
+import save from "./images/save.png"
+import save_active from "./images/save-active.png"
+import search from "./images/search.png"
+import search_active from "./images/search-active.png"
+import tweet from "./images/tweet.png"
+
+
+
+const IMAGES = {
+    home: home,
+    home_active: home_active,
+    list: list,
+    list_active: list_active,
+    message: message,
+    message_active: message_active,
+    more: more,
+    notification: notification,
+    notification_active: notification_active,
+    profile: profile,
+    profile_active: profile_active,
+    save: save,
+    save_active: save_active,
+    search: search,
+    search_active: search_active,
+    tweet: tweet,
+}
+export default IMAGES
