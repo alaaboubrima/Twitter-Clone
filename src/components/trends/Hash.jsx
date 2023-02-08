e79abe72cb79d9faa08e108983bd6b6a2aad37b9
alaaboubrima <97070504+alaaboubrima@users.noreply.github.com>
2023-02-08 22:28:37 +0100

    hash

---
+++ src/components/trends/Hash.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Hash.css"
+
+function Hash() {
+
+  return (
+    <div className='hashContainer'>
+      <span className='hash__location'>Trending in Algeria</span>
+      <span className='hash__name'>Something</span>
+      <span className='hash__tweets'>33K Tweets</span>
+    </div>
+  )
+}
+
+export default Hash
