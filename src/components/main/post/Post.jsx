afdd2f7078d7cad03b25f6666a6b3d0430e96c0a
alaaboubrima <97070504+alaaboubrima@users.noreply.github.com>
2023-02-09 22:44:54 +0100

    Update Post.jsx

--- src/components/main/post/Post.jsx
+++ src/components/main/post/Post.jsx
@@ -6,38 +6,55 @@ import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
 import RepeatIcon from "@material-ui/icons/Repeat";
 import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
 import PublishIcon from "@material-ui/icons/Publish";
+import axios from 'axios'
 
 function Post (props) {
+  const [post, setPost] = React.useState([])
 
-    return (
-      <div className="post">
-        <div className="post__avatar">
-          <Avatar src={props.avatar} />
-        </div>
-        <div className="post__body">
-          <div className="post__header">
-            <div className="post__headerText">
-              <h3>
-                {props.displayName}{" "}
-                <span className="post__headerSpecial">
-                  {props.verified && <VerifiedUserIcon className="post__badge" />} @
-                  {props.username}
-                </span>
-              </h3>
+  React.useEffect(() => {
+    axios
+      .get('https://randomuser.me/api/')
+      .then(res => {
+        setPost(res.data.results)
+      })
+      .catch(err => {
+        console.log("err")
+      })
+  }, [])
+    return (<>
+      {
+        post.map((item) => (
+          <div className="post">
+          <div className="post__avatar">
+            <Avatar src={props.avatar} />
+          </div>
+          <div className="post__body">
+            <div className="post__header">
+              <div className="post__headerText">
+                <h3>
+                  {item.gender}{" "}
+                  <span className="post__headerSpecial">
+                    {props.verified && <VerifiedUserIcon className="post__badge" />} @
+                    {props.username}
+                  </span>
+                </h3>
+              </div>
+              <div className="post__headerDescription">
+                <p>{props.text}</p>
+              </div>
             </div>
-            <div className="post__headerDescription">
-              <p>{props.text}</p>
+            <img src={props.image} alt="" />
+            <div className="post__footer">
+              <ChatBubbleOutlineIcon fontSize="small" />
+              <RepeatIcon fontSize="small" />
+              <FavoriteBorderIcon fontSize="small" />
+              <PublishIcon fontSize="small" />
             </div>
           </div>
-          <img src={props.image} alt="" />
-          <div className="post__footer">
-            <ChatBubbleOutlineIcon fontSize="small" />
-            <RepeatIcon fontSize="small" />
-            <FavoriteBorderIcon fontSize="small" />
-            <PublishIcon fontSize="small" />
-          </div>
         </div>
-      </div>
+        ))
+      }
+</>
     );
   }
 
