df50cadc57ea0b74168794f2d253b87769708c58
alaaboubrima <97070504+alaaboubrima@users.noreply.github.com>
2023-02-11 18:10:04 +0100

    Update Post.jsx

--- src/components/main/post/Post.jsx
+++ src/components/main/post/Post.jsx
@@ -15,32 +15,34 @@ function Post (props) {
     axios
       .get('https://randomuser.me/api/')
       .then(res => {
-        setPost(res.data.results)
+        setPost(res.data)
       })
       .catch(err => {
         console.log("err")
       })
   }, [])
+  console.log(post)
     return (<>
-      {
-        post.map((item) => (
+      { 
+      
+       post.results && post.results.map((item) => (
           <div className="post">
           <div className="post__avatar">
-            <Avatar src={props.avatar} />
+            <Avatar src={item.picture.large} />
           </div>
           <div className="post__body">
             <div className="post__header">
               <div className="post__headerText">
                 <h3>
-                  {item.gender}{" "}
+                  {item.name.first}{" "}{item.name.last}
                   <span className="post__headerSpecial">
                     {props.verified && <VerifiedUserIcon className="post__badge" />} @
-                    {props.username}
+                    {item.login.username}
                   </span>
                 </h3>
               </div>
               <div className="post__headerDescription">
-                <p>{props.text}</p>
+                <p>{"Hi, I am from "}{item.location.street.name}{". And this is just a random post."}</p>
               </div>
             </div>
             <img src={props.image} alt="" />
